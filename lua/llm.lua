7f03147a2f7a9f61fc90bb2585d4c91d36878326
Chunt0 <chunt.mail@gmail.com>
2024-07-26 22:21:40 -0700

    2024-07-26 22:21:40

--- lua/llm.lua
+++ lua/llm.lua
@@ -103,6 +103,7 @@ function M.make_ollama_spec_curl_args(opts, prompt, system_prompt)
 		stream = true,
 	}
 	local args = { "-N", "-X", "POST", "-H", "Content-Type: application/json", "-d", vim.json.encode(data) }
+	print(args)
 	table.insert(args, url)
 	return args
 end
