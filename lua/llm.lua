787a74f16a54de792a89f0250079571e40143704
Chunt0 <chunt.mail@gmail.com>
2024-07-28 15:25:10 -0700

    2024-07-28 15:25:10

--- lua/llm.lua
+++ lua/llm.lua
@@ -232,6 +232,7 @@ end
 local assistant_response = ""
 
 function M.handle_openai_spec_data(data_stream)
+	print("in handle openai spec data")
 	if data_stream:match('"delta":') then
 		local json = vim.json.decode(data_stream)
 		if json.choices and json.choices[1] and json.choices[1].delta then
