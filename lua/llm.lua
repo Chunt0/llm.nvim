eda80c8ce4f080ea07b534750a30d9ed32230a0d
Chunt0 <chunt.mail@gmail.com>
2024-07-29 13:08:24 -0700

    2024-07-29 13:08:24

--- lua/llm.lua
+++ lua/llm.lua
@@ -21,7 +21,6 @@ end
 local function trim_context(context, max_length)
 	local len = #context
 	if len > max_length then
-		print("trimming context")
 		-- Calculate the number of elements to remove
 		local remove_count = len - max_length
 		-- Remove the first `remove_count` elements from the context
@@ -160,6 +159,47 @@ function M.make_openai_spec_curl_args(opts, prompt, system_prompt)
 	return args
 end
 
+local groq_messages = {}
+local groq_count = 0
+
+function M.make_groq_spec_curl_args(opts, prompt, system_prompt)
+	local url = opts.url
+	local api_key = opts.api_key_name and get_api_key(opts.api_key_name)
+	local data = nil
+	if groq_count == 0 then
+		local message = { { role = "system", content = system_prompt }, { role = "user", content = prompt } }
+		data = {
+			messages = message,
+			model = opts.model,
+			temperature = 0.7,
+			stream = true,
+		}
+		for _, v in pairs(message) do
+			table.insert(groq_messages, v)
+		end
+		groq_count = 1
+	elseif groq_count == 1 then
+		local next_message = { role = "user", content = prompt }
+		table.insert(groq_messages, next_message)
+		data = {
+			messages = groq_messages,
+			model = opts.model,
+			temperature = 0.7,
+			stream = true,
+		}
+	end
+
+	local args = { "-N", "-X", "POST", "-H", "Content-Type: application/json", "-d", vim.json.encode(data) }
+	if api_key then
+		table.insert(args, "-H")
+		table.insert(args, "Authorization: Bearer " .. api_key)
+	end
+	table.insert(args, url)
+	return args
+end
+
+--[[
+
 function M.make_groq_spec_curl_args(opts, prompt, system_prompt)
 	local url = opts.url
 	local api_key = opts.api_key_name and get_api_key(opts.api_key_name)
@@ -177,6 +217,7 @@ function M.make_groq_spec_curl_args(opts, prompt, system_prompt)
 	table.insert(args, url)
 	return args
 end
+]]
 
 local context = {}
 local max_length = 25000
