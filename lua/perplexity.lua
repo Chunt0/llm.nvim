4801e090ff5db0d228c545251db69d2d47c3fad8
Chunt0 <chunt.mail@gmail.com>
2024-07-30 22:27:50 -0700

    2024-07-30 22:27:50

--- lua/perplexity.lua
+++ lua/perplexity.lua
@@ -1,8 +1,8 @@
 local llm = require("llm")
 local prompts = require("prompts")
 
-local PERPLEXITY_URL = "https://api.groq.com/openai/v1/chat/completions"
-local PERPLEXITY_MODEL = "llama-3.1-70b-versatile"
+local PERPLEXITY_URL = "https://api.perplexity.ai/chat/completions"
+local PERPLEXITY_MODEL = "llama-3.1-sonar-large-128k-chat"
 local PERPLEXITY_API_KEY = "PERPLEXITY_API_KEY"
 local FRAMEWORK = "PERPLEXITY"
 
