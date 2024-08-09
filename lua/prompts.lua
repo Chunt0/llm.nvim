c9b29967e1835399a45e285da17dc9bb6d6ce67a
Chunt0 <chunt.mail@gmail.com>
2024-08-09 08:48:42 -0700

    2024-08-09 08:48:42

--- lua/prompts.lua
+++ lua/prompts.lua
@@ -1,5 +1,5 @@
 local prompts = {
-	empty_prompt = "",
+	prompt = "",
 	code_prompt = "You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks",
 	helpful_prompt = "You are a helpful assistant. What I have sent are my notes so far. You are very curt, yet helpful. You will always adjust your attitude as I request it.",
 	en2ch_prompt = "You are a helpful assistant. Your goal is to translate text. You will never add anything to the text or output and you will never add commentary about the text you generate. Translate the following into Chinese: ",
