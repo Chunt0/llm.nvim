1c396f8df27a6141645e2759127d0dac160f77fd
Chunt0 <chunt.mail@gmail.com>
2024-08-16 17:19:44 -0700

    2024-08-16 17:19:44

--- lua/models.lua
+++ lua/models.lua
@@ -3,7 +3,7 @@ local models = {
 	openai = "gpt-4o",
 	anthropic = "claude-3-5-sonnet-20240620",
 	groq = "llama-3.1-70b-versatile",
-	ollama = "llama3.1:70b-instruct-q2_k",
+	ollama = "llama3.1:70b-instruct-q4_0",
 }
 
 return models
