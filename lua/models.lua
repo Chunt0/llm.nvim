09fc5b381f1fdba4eb1b3412ee0cd3af59a2fe33
Chunt0 <chunt.mail@gmail.com>
2025-01-01 14:07:47 -0800

    2025-01-01 14:07:47

--- lua/models.lua
+++ lua/models.lua
@@ -1,9 +1,9 @@
 local models = {
 	perplexity = "llama-3.1-sonar-large-128k-chat",
 	openai = "gpt-4o",
-	anthropic = "claude-3-5-sonnet-20240620",
-	groq = "llama-3.1-70b-versatile",
-	ollama = "llama3.1:70b-instruct-q4_0",
+	anthropic = "claude-3-5-sonnet-latest",
+	groq = "llama-3.3-70b-versatile",
+	ollama = "llama3.3:latest",
 }
 
 return models
